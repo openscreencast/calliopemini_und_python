221937fbebe6e7709578ed48de402363ce5e8040
openscreencast <info@openscreencast.de>
2017-09-10 17:24:22 +0200

    update Letsgo - App

--- book/_book/js/letsgo_app.js
+++ book/_book/js/letsgo_app.js
@@ -1,8 +1,8 @@
-tools = ["Rucksack", "Zubehörbox"];
+tools = ["Rucksack"];
 
 function sucheSchritt(stelle) {
 
-	if(stelle == 9 && tools.length == 5) { sucheSchritt(12); return }
+	if(stelle == 9 && tools.length == 4) { sucheSchritt(12); return }
 
 	for(var i = 0; i < schritte.length; i++) {
  		if(schritte[i].id == stelle) {
